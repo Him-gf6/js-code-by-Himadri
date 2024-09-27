9acb4f31c862f86833d345a24995aa2a5b0dfbe6
Him-gf6 <159515063+Him-gf6@users.noreply.github.com>
2024-09-27 14:45:46 +0000

    variable.js

---
+++ 01_basics/variable.js
@@ -0,0 +1,6 @@
+const account_id=12345
+let account_email="1234@gmail.com"
+var account_password=123123
+
+account_city="Bongaigaon"
+console.table([account_id,account_email,account_password,account_city]);
