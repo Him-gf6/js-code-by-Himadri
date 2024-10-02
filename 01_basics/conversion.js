3f55323a6dd1f51ef792ade56a1f70a0fb9f33e3
Him-gf6 <159515063+Him-gf6@users.noreply.github.com>
2024-10-02 14:56:33 +0000

    conversion of data types

---
+++ 01_basics/conversion.js
@@ -0,0 +1,11 @@
+let score="33abc"
+
+//console.log(typeof score)
+
+let makeitinvariable=Number(score)
+//console.log(makeitinvariable)
+
+let thenumberis=1
+
+let makeitinboolean=Boolean(thenumberis)
+console.log(makeitinboolean)
