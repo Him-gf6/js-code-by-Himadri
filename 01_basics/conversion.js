773954305c24c05d05b7d47ff6d500e1841f2619
Him-gf6 <159515063+Him-gf6@users.noreply.github.com>
2024-10-07 15:31:30 +0000

    data type comparision

--- 01_basics/conversion.js
+++ 01_basics/conversion.js
@@ -8,4 +8,25 @@ let makeitinvariable=Number(score)
 let thenumberis=1
 
 let makeitinboolean=Boolean(thenumberis)
-console.log(makeitinboolean)
+//console.log(makeitinboolean)
+
+let number=55
+
+let changenumber=String(number)
+//console.log(changenumber)
+//console.log(typeof changenumber)
+
+//**************** operations ******************/
+
+let value=3
+let negvalue= -value
+//console.log(negvalue)
+
+
+let age=45
+
+let stringage=String(age)
+console.log(stringage)
+console.log(typeof stringage);
+
+
