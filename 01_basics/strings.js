ea22da05885140a097181a4ca021ee126417f6c4
Him-gf6 <159515063+Him-gf6@users.noreply.github.com>
2024-10-10 14:07:28 +0000

    string in js

---
+++ 01_basics/strings.js
@@ -0,0 +1,21 @@
+const name="himadri"
+const repocount=20
+
+console.log(`Hello my name is ${name} and i have a total of ${repocount} repocount`);
+
+const gfg = new String(`himadri`)
+
+console.log(gfg[4]);
+console.log(gfg.__proto__);
+
+console.log(gfg.length);
+console.log(gfg.toUpperCase());
+console.log(gfg.charAt(4));
+console.log(gfg.indexOf(`i`));
+
+console.log(gfg.substring(1,5));
+console.log(gfg.slice(-3,5));
+
+const erd="   rahul    "
+console.log(erd);
+console.log(erd.trim());
