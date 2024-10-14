7eaa40f26d3bc7c8602ee927642b99c1b9ca64a9
Him-gf6 <159515063+Him-gf6@users.noreply.github.com>
2024-10-14 14:56:31 +0000

    numbers and maths

---
+++ 01_basics/numbers_and_maths.js
@@ -0,0 +1,32 @@
+const score=400;
+//console.log(score);
+
+const balance=new Number(100)
+//console.log(balance);
+
+//console.log(balance.toFixed(4));
+//console.log(balance);
+
+const othername=111123.8966
+//console.log(othername.toPrecision(4));
+
+const hundreds=10000000
+//console.log(hundreds.toLocaleString('en-IN'));
+
+
+//*************************  Maths  *********************** //
+
+//console.log(Math)
+//console.log(Math.abs(-7));
+//console.log(Math.round(43.8));
+//console.log(Math.floor(5));
+
+console.log(Math.random());
+console.log(Math.floor(Math.random()*10+1));
+console.log(Math.random());
+
+
+const max=30
+const min=20
+
+console.log(Math.floor(Math.random(max-min)*10+min));
