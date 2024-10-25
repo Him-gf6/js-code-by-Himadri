c090e0aad34c4916ea4fde93ed3b5ebc693ff604
Him-gf6 <159515063+Him-gf6@users.noreply.github.com>
2024-10-25 06:18:36 +0000

    scopes in javascript

---
+++ 01_basics/scopes.js
@@ -0,0 +1,17 @@
+ function one(){
+    const username="Himadri"
+
+    function two(){
+        const channelname="chaiaurcode"
+        console.log(channelname);
+        console.log(username);
+    }
+    //console.log(channelname);
+    
+
+    two()
+    //console.log(username);
+    
+ }
+
+ one()
