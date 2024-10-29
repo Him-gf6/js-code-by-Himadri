0d433edc37bfe43a8ce7f9230bccba395dc497ca
Him-gf6 <159515063+Him-gf6@users.noreply.github.com>
2024-10-29 14:57:36 +0000

    if,else and elseif statement

---
+++ 01_basics/if_else_statement.js
@@ -0,0 +1,38 @@
+// // if
+
+// const isuserliggesin=true
+// const temperature=41
+
+
+// if(temperature===41){
+//     console.log("yes,it is");
+// }
+// else{
+//     console.log("no,it is not");
+// }
+
+
+// const score=200
+
+// if(score>100){
+//     let power="fly"
+//     console.log(`user power:${power}`);   
+// }
+
+// console.log(`user power:${power}`); 
+
+
+const isuserloggedin=true
+const debitcard=true
+const isuserloggedinbygoogle=false
+const isuserloggedinbyemail=true
+
+// if(isuserloggedin && debitcard ){
+//     console.log("yes the user is logged");
+    
+// }
+
+if(isuserloggedinbygoogle || isuserloggedinbyemail ){
+    console.log("yes the user is logged");
+    
+}
