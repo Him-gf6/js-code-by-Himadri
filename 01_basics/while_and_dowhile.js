319a93af7898fa5a6d4cfb702001e5442669824d
Him-gf6 <159515063+Him-gf6@users.noreply.github.com>
2024-10-31 15:31:20 +0000

    while and do_while

---
+++ 01_basics/while_and_dowhile.js
@@ -0,0 +1,24 @@
+//while loop
+
+
+// let index=0
+// while (index<=10) {
+//     console.log(index); 
+//     index++;  
+// }
+
+const myarray=["flash","superman","batman"]
+let i=0
+while (i<myarray.length) {
+    console.log(myarray);
+    i++;
+}
+
+
+//do_while
+
+let score=1
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score<=10);
